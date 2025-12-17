dfb716a797fb22e151f697ff3a817208fddecc3f
gautam-kumar71 <gk903138@gmail.com>
2025-12-17 23:15:21 +0530

    [Gautam] : added age of Harry program

---
+++ gcr-codebase/programming-elements/level-1/AgeOfHarry.java
@@ -0,0 +1,15 @@
+//Create AgeOfHarry Class to calculate age of Harry
+public class AgeOfHarry{
+    public static void main(String args[])
+    {
+        //Create a variable currentYear and birthYear to store the current year
+        int currentYear=2024;
+        int birthYear=2000; 
+        
+        // Calculate Harry's age
+        int harryAge=currentYear-birthYear;
+        
+        //Display the calculated age of Harry
+        System.out.println("Harry's age in 2024 is "+harryAge);
+    }
+}
