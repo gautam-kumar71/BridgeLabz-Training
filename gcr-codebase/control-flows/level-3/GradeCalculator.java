ae2a7c16fd190991058a4be0df7c6d7db3ddf964
gautam-kumar71 <gk903138@gmail.com>
2025-12-19 09:24:58 +0530

    [Gautam] : added grade calculator program

---
+++ gcr-codebase/control-flows/level-3/GradeCalculator.java
@@ -0,0 +1,60 @@
+//Writing a Java Code to input marks and 3 subjects physics, chemistry and maths. Compute the percentage and then calculate the grade
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+		//Create Scanner object to take input from the user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Physics marks: ");
+        int physics = input.nextInt();
+        System.out.print("Enter Chemistry marks: ");
+        int chemistry = input.nextInt();
+        System.out.print("Enter Maths marks: ");
+        int maths = input.nextInt();
+
+        double average = (physics + chemistry + maths) / 3.0;
+        int range = (int) average / 10;
+         String grade;
+        String remarks;
+
+        // Grade calculation
+        if (average >= 80)
+		{
+            grade = "A";
+            remarks = "Level 4, above agency-normalized standards";
+        }
+		else if (average >= 70)
+		{
+            grade = "B";
+            remarks = "Level 3, at agency-normalized standards";
+        }
+		else if (average >= 60)
+		{
+            grade = "C";
+            remarks = "Level 2, below but approaching agency-normalized standards";
+        }
+		else if (average >= 50)
+		{
+            grade = "D";
+            remarks = "Level 1, well below agency-normalized standards";
+        }
+		else if (average >= 40)
+		{
+            grade = "E";
+            remarks = "Level 1-, too below agency-normalized standards";
+        }
+		else
+		{
+            grade = "R";
+            remarks = "Remedial standards";
+        }
+
+        // DIsplay the Result
+        System.out.println("\n--- Result ---");
+        System.out.println("Average Marks: " + average + "%");
+        System.out.println("Grade: " + grade);
+        System.out.println("Remarks: " + remarks);
+
+        input.close(); //Closing Scanner object
+    }
+}
