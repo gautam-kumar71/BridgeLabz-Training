da04b6220392a25569b213184239cd93875d744b
gautam-kumar71 <gk903138@gmail.com>
2025-12-22 00:02:40 +0530

    [Gautam] : added unit converter III program

---
+++ gcr-codebase/methods/level-2/UnitConvertorIII.java
@@ -0,0 +1,39 @@
+// Create utility class UnitConvertor to perform unit conversions
+public class UnitConvertor {
+
+    // Method to convert Fahrenheit to Celsius
+    public static double convertFarhenheitToCelsius(double farhenheit) {
+        double farhenheit2celsius = (farhenheit - 32) * 5 / 9;
+        return farhenheit2celsius;
+    }
+
+    // Method to convert Celsius to Fahrenheit
+    public static double convertCelsiusToFarhenheit(double celsius) {
+        double celsius2farhenheit = (celsius * 9 / 5) + 32;
+        return celsius2farhenheit;
+    }
+
+    // Method to convert pounds to kilograms
+    public static double convertPoundsToKilograms(double pounds) {
+        double pounds2kilograms = 0.453592;
+        return pounds * pounds2kilograms;
+    }
+
+    // Method to convert kilograms to pounds
+    public static double convertKilogramsToPounds(double kilograms) {
+        double kilograms2pounds = 2.20462;
+        return kilograms * kilograms2pounds;
+    }
+
+    // Method to convert gallons to liters
+    public static double convertGallonsToLiters(double gallons) {
+        double gallons2liters = 3.78541;
+        return gallons * gallons2liters;
+    }
+
+    // Method to convert liters to gallons
+    public static double convertLitersToGallons(double liters) {
+        double liters2gallons = 0.264172;
+        return liters * liters2gallons;
+    }
+}
