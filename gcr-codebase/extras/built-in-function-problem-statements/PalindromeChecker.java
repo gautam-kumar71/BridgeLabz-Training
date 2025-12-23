bcf2e2185cbcdedbc73447eca1c8a9df0be841b2
gautam-kumar71 <gk903138@gmail.com>
2025-12-23 23:35:03 +0530

    [Gautam] : added palindrome checker program

---
+++ gcr-codebase/extras/built-in-function-problem-statements/PalindromeChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    // Method to check palindrome
+    static boolean isPalindrome(String text) {
+        int start = 0, end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
