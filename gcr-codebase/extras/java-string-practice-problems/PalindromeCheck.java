6e024108cd36cc155b0d23e8336f0259cd3ab7b7
gautam-kumar71 <gk903138@gmail.com>
2025-12-23 23:10:30 +0530

    [Gautam] : added palindrome check program

---
+++ gcr-codebase/extras/java-string-practice-problems/PalindromeCheck.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+
+    // Method to check palindrome
+    static boolean isPalindrome(String text) {
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input))
+            System.out.println("Palindrome String");
+        else
+            System.out.println("Not a Palindrome String");
+    }
+}
