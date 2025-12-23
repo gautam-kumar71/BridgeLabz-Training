6011b56ac2136e4937bc9b7f588a0b708ac75fd3
gautam-kumar71 <gk903138@gmail.com>
2025-12-23 23:22:40 +0530

    [Gautam] : added date arithmetic program

---
+++ gcr-codebase/extras/built-in-function-practice-problems/DateArithmetic.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DateArithmetic {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take date input from user
+        System.out.print("Enter date (yyyy-mm-dd): ");
+        String inputDate = sc.nextLine();
+
+        // Convert input string to LocalDate
+        LocalDate date = LocalDate.parse(inputDate);
+
+        // Add 7 days
+        date = date.plusDays(7);
+
+        // Add 1 month
+        date = date.plusMonths(1);
+
+        // Add 2 years
+        date = date.plusYears(2);
+
+        // Subtract 3 weeks
+        date = date.minusWeeks(3);
+
+        // Display final date
+        System.out.println("Final Date after calculations: " + date);
+    }
+}
