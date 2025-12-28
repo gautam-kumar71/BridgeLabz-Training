2d0bc2a0d0f4f130039bb4391335eab0abfd6748
gautam-kumar71 <gk903138@gmail.com>
2025-12-28 22:29:01 +0530

    [Gautam] : added first missing positive program

---
+++ leet-code-codebase/array/FirstMissingPositive.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Place each number in its correct position if possible
+        for (int i = 0; i < n; i++) {
+            // while current number is in range [1, n] and not in correct position
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                // Swap nums[i] with the element at its target position
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        // Step 2: Find the first position where index + 1 != value
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1; // This is the smallest missing positive
+            }
+        }
+
+        // If all numbers from 1 to n are in place, return n + 1
+        return n + 1;
+    }
+}
