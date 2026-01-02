3a32bf986a107134f7749409c8cb4e92b21e22ec
gautam-kumar71 <gk903138@gmail.com>
2026-01-02 11:52:58 +0530

    [Gautam] : added library management system program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulation/librarymanagementsystem/Reservable.java
@@ -0,0 +1,10 @@
+package com.encapsulation.librarymanagementsystem;
+
+interface Reservable {
+
+    void reserveItem(String borrowerName);
+
+    boolean checkAvailability();
+
+    String getBorrowerInfo();
+}
