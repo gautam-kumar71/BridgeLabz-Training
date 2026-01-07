5225850a90f775be8bc32a49b012365293bf8167
gautam-kumar71 <gk903138@gmail.com>
2026-01-08 00:06:06 +0530

    [Gautam] : added Convert Byte Stream to Character Stream Using InputStreamReader program

---
+++ dsa-practice/gcr-codebase/DSAConcepts/src/com/ioandstringsandsearching/inputstreamreader/problem1/InputStreamReadMain.java
@@ -0,0 +1,11 @@
+package com.ioandstringsandsearching.inputstreamreader.problem1;
+
+public class InputStreamReadMain {
+
+    public static void main(String[] args) {
+
+        String filePath = "src/com/ioandstringsandsearching/inputstreamreader/problem1/sample.txt";
+
+        InputStreamRead.readFileUsingInputStreamReader(filePath);
+    }
+}
