793ff7544b6be848f31c2a974a15894b897614ff
gautam-kumar71 <gk903138@gmail.com>
2026-01-08 00:06:34 +0530

    [Gautam] : added Read User Input and Write to File Using InputStreamReader program

---
+++ dsa-practice/gcr-codebase/DSAConcepts/src/com/ioandstringsandsearching/inputstreamreader/problem2/ConsoleToFileMain.java
@@ -0,0 +1,11 @@
+package com.ioandstringsandsearching.inputstreamreader.problem2;
+
+public class ConsoleToFileMain {
+
+    public static void main(String[] args) {
+
+        String filePath = "src/com/ioandstringsandsearching/inputstreamreader/problem2/output.txt";
+
+        ConsoleToFile.readFromConsoleAndWriteToFile(filePath);
+    }
+}
