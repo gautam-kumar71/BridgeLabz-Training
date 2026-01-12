6659c65ef865689109b574cbb0a1959ba281e66f
gautam-kumar71 <gk903138@gmail.com>
2026-01-12 12:17:52 +0530

    [Gautam] : added LeetCode java solution for Happy number problem

---
+++ java-oops-practice/leetcode-codebase/finalandstatic/LC0342_PowerOfFour.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LC0342_PowerOfFour {
+
+    // Checks if the number is a power of four
+    public boolean isPowerOfFour(int n) {
+
+        // Must be positive and power of two
+        if (n <= 0 || (n & (n - 1)) != 0) {
+            return false;
+        }
+
+        // Only one bit set at even position
+        return (n & 0x55555555) != 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        LC0342_PowerOfFour sol = new LC0342_PowerOfFour();
+
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+
+        System.out.println(sol.isPowerOfFour(n));
+
+        input.close();
+    }
+}
