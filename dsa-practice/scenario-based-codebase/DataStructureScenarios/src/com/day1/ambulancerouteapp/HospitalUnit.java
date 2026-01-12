0696833d169174a5ebd20be0175845969fdbf4c3
gautam-kumar71 <gk903138@gmail.com>
2026-01-12 11:41:10 +0530

    [Gautam] : Added AmbulanceRoute – Emergency Patient Navigation java files

---
+++ dsa-practice/scenario-based-codebase/DataStructureScenarios/src/com/day1/ambulancerouteapp/HospitalUnit.java
@@ -0,0 +1,18 @@
+package com.day1.ambulancerouteapp;
+
+public class HospitalUnit {
+	String name;
+	boolean isAvailable;
+	HospitalUnit next;
+
+	// constructor
+	public HospitalUnit(String name, boolean isAvailable) {
+		this.name = name;
+		this.isAvailable = isAvailable;
+		this.next = null;
+	}
+
+	public String getStatus() {
+		return isAvailable ? "Available" : "Busy";
+	}
+}
