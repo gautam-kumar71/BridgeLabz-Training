aaefc31ed5016728a1890fb038696d46f9c6c936
gautam-kumar71 <gk903138@gmail.com>
2026-01-15 12:54:07 +0530

    [Gautam] : added LeetCode java solution for Three Sum problem

---
+++ dsa-practice/leetcode-codebase/arrays/LC0015_ThreeSum.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class LC0015_ThreeSum {
+
+    // Finds all triplets whose sum is zero
+    public void threeSum(int[] nums) {
+
+        // Sort array using simple bubble sort
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
+        // Use two pointers for each number
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    System.out.println(nums[i] + " " + nums[left] + " " + nums[right]);
+
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        LC0015_ThreeSum sol = new LC0015_ThreeSum();
+
+        System.out.print("Enter array size: ");
+        int n = input.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = input.nextInt();
+        }
+
+        sol.threeSum(nums);
+
+        input.close();
+    }
+}
