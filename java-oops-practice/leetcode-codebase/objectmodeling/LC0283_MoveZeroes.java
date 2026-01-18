ad63435d6f8f9b3d14eddf0952bcb7b2c8f0ac4c
gautam-kumar71 <gk903138@gmail.com>
2026-01-19 00:18:57 +0530

    [Gautam] : added LeetCode java solution for Move Zeroes problem

---
+++ java-oops-practice/leetcode-codebase/objectmodeling/LC0283_MoveZeroes.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class LC0283_MoveZeroes {
+
+    // Moves all zeros to the end while keeping order
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        // Place non-zero elements first
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index++] = num;
+            }
+        }
+
+        // Fill remaining positions with zero
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        LC0283_MoveZeroes sol = new LC0283_MoveZeroes();
+
+        System.out.print("Enter array size: ");
+        int n = input.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = input.nextInt();
+        }
+
+        sol.moveZeroes(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        input.close();
+    }
+}
